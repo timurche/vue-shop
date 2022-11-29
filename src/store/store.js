c563a68928e4a0c5180b832eb565661c0e844af8
timurche <63548007+timurche@users.noreply.github.com>
2022-11-29 16:51:55 +0500

    vuex + axios

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,35 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 let store = new Vuex.Store({
-  state: { fuck: "you" },
-  mutations: {},
-  getters: {},
-  actions: {}
+  state: {
+    products: []
+  },
+  mutations: {
+    setProductsToState: (state, products) => {
+      state.products = products;
+    }
+  },
+  actions: {
+    fetchProducts({ commit }) {
+      return axios("http://localhost:3000/products", {
+        method: "GET"
+      })
+        .then((products) => {
+          commit("setProductsToState", products.data);
+          return products;
+        })
+        .catch((e) => {
+          console.log(e);
+          return e;
+        });
+    }
+  },
+  getters: {
+    PRODUCTS(state) {
+      return state.products;
+    }
+  }
 });
 
 export default store;
